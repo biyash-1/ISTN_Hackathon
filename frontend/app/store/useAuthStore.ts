fc9f9643a36f342e28871feb4c42a59e59397512
biyash-1 <sthaalish39@gmail.com>
2025-12-14 03:14:54 +0545

    fix

--- frontend/app/store/useAuthStore.ts
+++ frontend/app/store/useAuthStore.ts
@@ -1,5 +1,7 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { logoutUser } from "@/services/auth";
+
 
 interface User {
   email: string;
@@ -10,30 +12,43 @@ interface AuthState {
   isAuthenticated: boolean;
   token: string | null;
   login: (user: User, token: string) => void;
-  logout: () => void;
+  logout: () => Promise<void>;
 }
 
+
 export const useAuthStore = create<AuthState>()(
+
   persist(
     (set) => ({
       user: null,
       isAuthenticated: false,
       token: null,
+
       login: (user, token) =>
         set({
           user,
           token,
           isAuthenticated: true,
         }),
-      logout: () =>
-        set({
-          user: null,
-          token: null,
-          isAuthenticated: false,
-        }),
+
+      logout: async () => {
+        try {
+          await logoutUser(); 
+  
+        } catch (err) {
+          console.error("Logout API failed", err);
+        } finally {
+     
+          set({
+            user: null,
+            token: null,
+            isAuthenticated: false,
+          });
+        }
+      },
     }),
     {
-      name: 'auth-storage', // name in localStorage
+      name: "auth-storage",
     }
   )
 );
